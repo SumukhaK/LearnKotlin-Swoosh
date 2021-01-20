777e019ae6038d6f2e130e4d0f2ce6becfc607a5
IamSumukha <sumukh.aithal@gmail.com>
2021-01-20 20:47:20 +0530

    skill activity added

---
+++ app/src/main/java/com/johnnybkotlin/swoosh/BaseActivity.kt
@@ -0,0 +1,45 @@
+package com.johnnybkotlin.swoosh
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+open class BaseActivity : AppCompatActivity() {
+
+    val TAG = "LifeCycle"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d(TAG,"${javaClass.simpleName} is in onCreate")
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onStart() {
+        Log.d(TAG,"${javaClass.simpleName} is in onStart()")
+        super.onStart()
+    }
+
+    override fun onResume() {
+        Log.d(TAG,"${javaClass.simpleName} is in onResume()")
+        super.onResume()
+    }
+
+    override fun onPause() {
+        Log.d(TAG,"${javaClass.simpleName} is in onResume()")
+        super.onPause()
+    }
+
+    override fun onRestart() {
+        Log.d(TAG,"${javaClass.simpleName} is in onRestart()")
+        super.onRestart()
+    }
+
+    override fun onStop() {
+        Log.d(TAG,"${javaClass.simpleName} is in onStop()")
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        Log.d(TAG,"${javaClass.simpleName} is in onDestroy()")
+        super.onDestroy()
+    }
+}
--- app/src/main/java/com/johnnybkotlin/swoosh/WelcomeActivity.kt
+++ app/src/main/java/com/johnnybkotlin/swoosh/WelcomeActivity.kt
@@ -1,11 +1,23 @@
 package com.johnnybkotlin.swoosh
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
-class WelcomeActivity : AppCompatActivity() {
+class WelcomeActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        get_started_button.setOnClickListener {
+            //Toast.makeText(this@WelcomeActivity, "You clicked me.", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this@WelcomeActivity, LeagueActivity::class.java);
+            startActivity(intent);
+        }
+
+
     }
 }
+
+
