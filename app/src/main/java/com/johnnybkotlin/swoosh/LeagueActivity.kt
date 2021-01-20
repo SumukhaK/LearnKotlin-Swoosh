a4a139994a15bcfb808ebf9013712cec057d7ff4
IamSumukha <sumukh.aithal@gmail.com>
2021-01-20 23:08:20 +0530

    added some coding logics and finish activity

--- app/src/main/java/com/johnnybkotlin/swoosh/LeagueActivity.kt
+++ app/src/main/java/com/johnnybkotlin/swoosh/LeagueActivity.kt
@@ -3,16 +3,49 @@ package com.johnnybkotlin.swoosh
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_league.*
 
 class LeagueActivity : BaseActivity() {
+
+    var selectedLeague = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_league)
 
+
         league_next_btn.setOnClickListener {
-            val intent = Intent(this@LeagueActivity, SkillActivity::class.java);
-            startActivity(intent);
+            if(selectedLeague != "") {
+                val skilledIntent = Intent(this@LeagueActivity,SkillActivity::class.java);
+                skilledIntent.putExtra("League",selectedLeague);
+                startActivity(skilledIntent)
+            }else{
+                Toast.makeText(this@LeagueActivity,"Please select a League",Toast.LENGTH_LONG).show()
+            }
         }
+    }
+
+    fun onCoedClicked(view: View) {
+
+        mens_league_btn.isChecked = false
+        womens_league_btn.isChecked = false
+        selectedLeague = "Co-ed"
+
+    }
+    fun onWomensClicked(view: View) {
+
+        mens_league_btn.isChecked = false
+        coed_league_btn.isChecked = false
+        selectedLeague = "Women's"
+
+    }
+    fun onMensClicked(view: View) {
+
+        womens_league_btn.isChecked = false
+        coed_league_btn.isChecked = false
+        selectedLeague = "Men's"
+
     }
 }
--- app/src/main/java/com/johnnybkotlin/swoosh/SkillActivity.kt
+++ app/src/main/java/com/johnnybkotlin/swoosh/SkillActivity.kt
@@ -2,10 +2,24 @@ package com.johnnybkotlin.swoosh
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
-class SkillActivity : AppCompatActivity() {
+class SkillActivity : BaseActivity() {
+
+    var league = "";
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_skill)
+
+        league = intent.getStringExtra("League").toString();
+
+    }
+
+    fun onBeginnerlicked(view: View) {
+        Toast.makeText(this," I'm a Beginner in the ${league} league",Toast.LENGTH_LONG).show()
+    }
+    fun onBallerlicked(view: View) {
+        Toast.makeText(this," I'm a Baller in the ${league} league",Toast.LENGTH_LONG).show()
     }
 }
