6b413dd4bcbf3e10711b898248ad04fc45476a4a
IamSumukha <sumukh.aithal@gmail.com>
2021-01-20 23:51:58 +0530

    adding a comment

--- app/src/main/java/com/johnnybkotlin/swoosh/FinishActivity.kt
+++ app/src/main/java/com/johnnybkotlin/swoosh/FinishActivity.kt
@@ -2,10 +2,20 @@ package com.johnnybkotlin.swoosh
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_finish.*
 
 class FinishActivity : AppCompatActivity() {
+
+    var league = "";
+    var skills = "";
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_finish)
+
+        league = intent.getStringExtra("League").toString()
+        skills = intent.getStringExtra("skill").toString()
+
+
+        selection_textview.setText("Looking for a ${league} ${skills} league around you...")
     }
 }
--- app/src/main/java/com/johnnybkotlin/swoosh/SkillActivity.kt
+++ app/src/main/java/com/johnnybkotlin/swoosh/SkillActivity.kt
@@ -1,13 +1,16 @@
 package com.johnnybkotlin.swoosh
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_skill.*
 
 class SkillActivity : BaseActivity() {
 
     var league = "";
+    var skills = "";
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_skill)
@@ -17,9 +20,25 @@ class SkillActivity : BaseActivity() {
     }
 
     fun onBeginnerlicked(view: View) {
-        Toast.makeText(this," I'm a Beginner in the ${league} league",Toast.LENGTH_LONG).show()
+        skills = "beginner"
+        baller_btn.isChecked = false
+        //Toast.makeText(this," I'm a Beginner in the ${league} league",Toast.LENGTH_LONG).show()
     }
     fun onBallerlicked(view: View) {
-        Toast.makeText(this," I'm a Baller in the ${league} league",Toast.LENGTH_LONG).show()
+        skills = "baller"
+        beginner_btn.isChecked = false
+        //Toast.makeText(this," I'm a Baller in the ${league} league",Toast.LENGTH_LONG).show()
+    }
+
+    fun onskillFinishClicked(view: View) {
+
+        if(skills != "") {
+            val intent = Intent(this@SkillActivity, FinishActivity::class.java);
+            intent.putExtra("League",league)
+            intent.putExtra("skill",skills)
+            startActivity(intent);
+        }else{
+            Toast.makeText(this," Please select a skill level..",Toast.LENGTH_LONG).show()
+        }
     }
 }
